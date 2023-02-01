19e8a390147ed9490560e84e54c70e5a13d466e1
LilDad <bezhuraedgar@gmail.com>
2023-02-02 01:44:29 +0200

    2

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,13 +1,3 @@
 // const { browser } = require("protractor");
 
 exports.config = {
-    specs: ['./specs/**/*.spec.js'],
-    framework: 'mocha',
-    mochaOpts: {
-        timeout: 30 * 1000
-    },
-    onPrepare() {
-        browser.waitForAngularEnabled(false)
-    },
-    SELENIUM_PROMISE_MANAGER: false
-};
